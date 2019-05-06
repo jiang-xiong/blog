e431b110f6422722fd7241187a27598712ce0403
jiang-xiong <jiangxiongjvn@outlook.com>
2019-05-06 08:04:34 +0800

    Create 8.函数节流.js

---
+++ 8.函数节流.js
@@ -0,0 +1,23 @@
+// 在时间间隔内无法被触发
+// 类似技能 cd
+var throttle = function (callBack, interval) {
+  // 一开始可执行
+  var cd = true
+  return function () {
+    if (cd === false) {
+      return
+    } else {
+      callBack()
+      // 函数调用后修改 cd, 回调不能执行
+      cd = false
+      setTimeout(function () {
+        // 时间间隔后修改 cd，回调可以执行
+        cd = true
+      }, interval)
+    }
+  }
+}
+
+document.addEventListener('scroll', throttle(function () {
+  console.log('scroll')
+}, 3000))
