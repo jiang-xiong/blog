3a8154ec8c70ca31bd5b8128e89ac9f157733150
jiang-xiong <jiangxiongjvn@outlook.com>
2019-05-09 10:59:30 +0800

    update 10， 11

---
+++ 10.赋值和浅拷贝.js
@@ -0,0 +1,38 @@
+var shallowCopy = function(o) {
+  var hash = {}
+  for (var prop in o) {
+    if (o.hasOwnProperty(prop)) {
+      hash[prop] = o[prop]
+    }
+  }
+  return hash
+}
+// 什么是浅拷贝
+// 浅拷贝用于拷贝对象的第一层属性
+// 无法拷贝对象中子对象的属性
+// 例如
+var a = {
+  name: 'a',
+  obj: {
+    age: 18,
+  },
+}
+var b = shallowCopy(a)
+b.name = 'b'
+b.obj.age = 20
+// b 和 a 的 age 指向的是同一块内存
+// 他们会随一方的改变而改变
+console.log(a)
+console.log(b)
+
+// 什么是赋值
+// 赋值得到的新变量和原变量指向同一块内存
+// 例如
+var a = {
+  name: 'a',
+}
+var b = a
+b.name = 'b'
+// b 与 a 的值会随一方的改变而改变
+console.log(a)
+console.log(b)
--- 11.深拷贝.js
+++ 11.深拷贝.js
@@ -6,19 +6,35 @@ var deepCopy = function (o) {
 		o1 = []
 	} else if (o instanceof Object) {
 		o1 = {}
-	}
+	} else if(o instanceof Function){
+		object2 = eval(object.toString())
+  }
 	for(var k in o) {
 	 o1[k] = deepCopy(o[k])
  }
 	return o1
 }
+// 什么是深拷贝
+// 深拷贝用于拷贝对象的全部属性
+// 例如
+var a = {
+  name: 'a',
+  obj: {
+    age: 18,
+		fn: function greeting() {
+			console.log('hi a')
+		},
+  },
 
-var o = {
-  a: 1,
-  arr: [2, [3]],
 }
-var deepO = deepCopy(o)
-deepO.a = 2
-deepO.arr[1][0] = 4
-console.log(o)
-console.log(deepO)
+var b = deepCopy(a)
+b.name = 'b'
+b.obj.age = 20
+b.obj.fn = function greeting() {
+	console.log('你好 b')
+}
+// a 与 b 指向不同的内存
+console.log(a)
+console.log(b)
+a.obj.fn()
+b.obj.fn()
